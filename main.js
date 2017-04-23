e866bf38b5b6ea233b2bf251f751fd0446b610ea
Blake Schwartz <blakehschwartz@gmail.com>
2017-04-23 15:58:33 -0700

    Adds page rendering/screen capture at various viewports script

---
+++ main.js
@@ -0,0 +1,32 @@
+var casper = require('casper').create();
+var fs = require('fs');
+
+if (fs.exists('./config.json') && fs.exists('./data.json')) {
+    var data = require('./data.json');
+    var config = require('./config.json');
+} else {
+    casper.exit();
+}
+
+var urls = data.urls;
+var viewportSizes = config.viewportSizes;
+
+casper.start();
+
+var counter = 0;
+casper.repeat(viewportSizes.length, function() {
+    var viewportSize = viewportSizes[counter];
+    casper.viewport(viewportSize, 1000)
+        .each(urls, function(self, item, index) {
+            self.thenOpen(item, function() {
+                var title = this.getTitle();
+                console.log(title);
+                this.wait(2000, function() {
+                    this.capture('./images/screenshot_' + index + '_' + viewportSize + '.png');
+                });
+            });
+        });
+    counter++;
+});
+
+casper.run();
